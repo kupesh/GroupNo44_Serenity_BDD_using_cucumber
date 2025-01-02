d3dac22a316d7d2cd33cd5a13cb2d35f1cfbe351
Udana D. Kodikara <chipseyofficial@gmail.com>
2025-01-03 00:31:33 +0530

    Post - Api Testing init

--- IT-Quality-Assurance-Assignment-API-Testing/src/test/java/com/example/serenity/apitesting/models/Book.java
+++ IT-Quality-Assurance-Assignment-API-Testing/src/test/java/com/example/serenity/apitesting/models/Book.java
@@ -5,6 +5,32 @@ public class Book {
     private String title;
     private String author;
 
+    public Book() {}
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public Book(int id, String title, int tag) {
+        this.id = id;
+        switch (tag) {
+            case 1: {
+                this.title = title;
+                break;
+            }
+            case 2: {
+                this.author = title;
+                break;
+            }
+        }
+    }
+
+    public Book(int id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
     // Getters and setters
     public int getId() {
         return id;
