91b5af0182141af5b50b6b55a07f5d041b7b1d84
gamagegmp <malithipgamage@gmail.com>
2025-01-03 11:48:17 +0530

    Api Put Test init

--- IT-Quality-Assurance-Assignment-API-Testing/src/test/java/com/example/serenity/apitesting/utils/RequestHelper.java
+++ IT-Quality-Assurance-Assignment-API-Testing/src/test/java/com/example/serenity/apitesting/utils/RequestHelper.java
@@ -37,7 +37,6 @@ public class RequestHelper {
         RequestSpecification request = RestAssured.given()
                 .baseUri(this.baseUri)
                 .basePath(this.endpoint)
-                .header("Content-Type", "application/json")
                 .accept(ContentType.JSON);
 
 
